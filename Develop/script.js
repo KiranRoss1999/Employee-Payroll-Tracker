1208e5863d3d4593a0c125f1d947d5cd005438da
KiranRoss <120018062+KiranRoss@users.noreply.github.com>
2024-03-21 20:29:15 +1100

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,127 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+const collectEmployees = function() {
+  // TODO: Get user input to create and return an array of employee objects
+  const employeesArray = [];
+  let addAnotherEmployee = true;
+
+  while(addAnotherEmployee){
+    const firstName = prompt("Enter employees first name:")
+    const lastName = prompt("Enter employees last name:")
+    let salary = prompt("Enter employees salary:")
+  
+/* Convert salary to a number and chech if the amount is valid */
+    salary = isNaN(Number(salary)) ? 0 : Number(salary);
+
+/* This is to create an employee object */
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
+    };
+
+/* Adds employees details to the array */
+    employeesArray.push(employee);
+
+/* Ask user if they want to add another employee */
+    const continueAdding = confirm("Do you want to add another employee?")
+    if (!continueAdding){
+      addAnotherEmployee = false;
+    }
+}
+return employeesArray;
+}
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+  const totalSalary = employeesArray.reduce((acc, employee) => acc + employee.salary, 0);
+  const averageSalary = totalSalary / employeesArray.length;
+
+/* Logs average employee salary to console with required message */
+  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is: $${(totalSalary / employeesArray.length).toLocaleString(undefined, {maximumFractionDigits: 2})}`);
+}
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  // TODO: Select and display a random employee
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+  const randomEmployee = employeesArray[randomIndex];
+
+/* Logs random employee to the console with the required message */
+  console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`);
+  
+/* Sort employees alphabetically by last name */
+employeesArray.sort((a, b) => a.lastName.localeCompare(b.lastName));
+return employeesArray;
+  
+}
+
+
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
---
+++ README.md
@@ -0,0 +1,51 @@
+# Employee Payroll Tracker
+
+## Descirption
+This weeks challenge revolved around JavaScript. My challange was to create a basic pay roll tracker for employees that are to be entered by the user with a given salary. The Pay roll tracker then asks the user if they would like to add another employee or cancel. From here the tracker will list the given names of employees sorted in alphabetical order by last name.
+
+Once the console log is open you will find there will a table if 4 or more employees are entered, it will also pick a random employee as a prize winner and calculate the overall average salary.
+
+## User Story
+AS A payroll manager
+I WANT AN employee payroll tracker
+SO THAT I can see my employees' payroll data and properly budget for the company
+
+## Acceptance Criteria
+GIVEN an employee payroll tracker
+WHEN I click the "Add employee" button
+THEN I am presented with a series of prompts asking for first name, last name, and salary
+WHEN I finish adding an employee
+THEN I am prompted to continue or cancel
+WHEN I choose to continue
+THEN I am prompted to add a new employee
+WHEN I choose to cancel
+THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+
+## Installation 
+To run this project locally, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/KiranRoss1999/Professional-Portfolio.git`
+2. Navigate to the project's directory: `cd Professional-Portfolio`
+3. Open the index.html file in your browser.
+
+## Mock-Up
+
+The following animation shows the web application's appearance and functionality:
+
+![tracker demo](./Develop/images/demo%201.gif)
+
+![tracker demo 2](./Develop/images/demo%202.png)
+
+## Live Demo Link
+https://kiranross1999.github.io/Employee-Payroll-Tracker/
+
+## GitHub Link
+https://github.com/KiranRoss1999/Employee-Payroll-Tracker
+
+## Usage
+
+## Credits
+[MDN Web Docs - return](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return) by Mozilla Developer Network
+[MDN Web Docs - while](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while) by Mozilla Developer Network
+[MDN Web Docs - isNaN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN) by Mozilla Developer Network
+[W3Schools - JavaScript Tutorial](https://www.w3schools.com/js/) by W3Schools
